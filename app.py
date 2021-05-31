fd950b102203e8b882157d1d84052014e119c611
armaan <imaap143@gmail.com>
2021-05-31 12:05:48 +0530

    added update and delete operations

--- app.py
+++ app.py
@@ -1,21 +1,58 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
 
-all_posts = [
-    { 'title': 'Post 1', 'content': 'Post 1 ka content'},
-    { 'title': 'Post 2', 'content': 'Post 2 ka content'},
-    { 'title': 'Post 3', 'content': 'Post 2 ka content'},
-    { 'title': 'Post 4', 'content': 'Post 2 ka content'}
-    ]
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content  = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(20), nullable=False, default='N/A')
+    date_posted = db.Column(db.DateTime,nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return'Blog post: '+ str(self.id) + self.author
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/posts')
+@app.route('/posts', methods=["GET", "POST"])
 def getPosts():
+    if request.method == "POST":
+        post_title = request.form['title']
+        post_author = request.form['author']
+        post_content = request.form['content']
+        new_post = BlogPost(title=post_title, author=post_author, content=post_content)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        all_posts = BlogPost.query.all()
     return render_template("posts.html", posts=all_posts)
 
+@app.route('/posts/delete/<int:id>')
+def deletePost(id):
+    post = BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
+
+@app.route('/posts/edit/<int:id>', methods=["GET", "POST"])
+def updatePost(id):
+    if request.method == "POST":
+        post = BlogPost.query.get_or_404(id)
+        post.author = request.form['author']
+        post.title = request.form['title']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        post = BlogPost.query.get(id)
+        return render_template('edit.html', post=post)
 if __name__ == "__main__":
     app.run(debug=True)
